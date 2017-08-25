8e956faac6e3c3c0a8e28fb63d4b6fc82b3093f9
zjmatac <m15062202802@163.com>
2017-08-25 22:26:12 +0800

    lunbo

---
+++ musicapp/src/main.js
@@ -0,0 +1,13 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import 'common/stylus/index.styl'
+/* eslint-disable no-new */
+fastclick.attach(document.body)
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
